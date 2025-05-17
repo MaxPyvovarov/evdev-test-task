227eaafadb7bfad8ff28c3688c4a916c0501e2dd
Maksym Pyvovarov <max.pivovar@gmail.com>
2025-05-17 19:06:35 +0300

    Header -> desktop version

---
+++ assets/js/index.js
@@ -0,0 +1,27 @@
+window.addEventListener('DOMContentLoaded', () => {
+	//Mobile Backdrop
+	const backdrop = document.querySelector('.backdrop');
+	backdrop.addEventListener('click', () => {
+		toggleMobileMenu();
+	});
+
+	//Mobile menu
+	const hamburger = document.querySelector('.hamburger-menu');
+	const mobileMenu = document.querySelector('.mobile-menu');
+	hamburger.addEventListener('click', () => {
+		toggleMobileMenu();
+	});
+
+	const mobileServices = document.querySelector('.mobile-services-dropdown');
+	mobileServices.addEventListener('click', () => {
+		mobileServices.classList.toggle('active');
+	});
+
+	function toggleMobileMenu() {
+		hamburger.classList.toggle('active');
+		mobileMenu.classList.toggle('active');
+		backdrop.classList.toggle('mobile-open');
+		document.querySelector('body').classList.toggle('backdrop-opened');
+		mobileServices.classList.remove('active');
+	}
+});
